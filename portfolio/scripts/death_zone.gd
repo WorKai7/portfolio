509263f953866376464e1b59e2b25b647b8cbcbf
WorKai <vaje2005@gmail.com>
2026-02-17 18:10:33 +0100

    feat(map, parkour, deathzones): Changed background, floor, added parkour, and added signs

---
+++ portfolio/scripts/death_zone.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var respawn_pos: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	body.position = respawn_pos
+	print("ON ENTRE")
+	print(body)
